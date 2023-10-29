ce6679476ffe3a7546f044663c7a55441decb83c
Monika Wiśniewska <wisniewska.monika00@gmail.com>
2023-10-29 02:18:14 +0200

    update certificates

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -31,7 +31,15 @@ module.exports = {
   ],
   positions: [
     {
-      title: 'Hamlet',
+      title: 'Type Annotations in Python - Training - Udemy [2023]',
+      period: '1595–1596, with a possible early draft written in 1591',
+      skills: ['tragedy', 'death', 'love'],
+      contents: `
+In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+`
+    },
+    {
+      title: 'Summer of Code - Challenge (Python, HTML, CSS, WebScraping, API) - FlyNerd [2023]',
       period: 'Likely early 17th century',
       skills: ['tragedy', 'philosophy', 'death'],
       contents: `
@@ -39,7 +47,15 @@ Prince Hamlet is visited by his father's ghost and ordered to avenge his father'
 `
     },
     {
-      title: 'Twelfth Night',
+      title: 'Elements of AI - Course - Univeristy of Helsinki/Jagiellonian University [2023]',
+      period: '1595–1596, with a possible early draft written in 1591',
+      skills: ['tragedy', 'death', 'love'],
+      contents: `
+In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+`
+    },
+    {
+      title: 'New You in IT - Challenge - COI [2023]',
       period: '1600 – 1601',
       skills: ['comedy', 'love'],
       contents: `
@@ -47,13 +63,13 @@ Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebast
 `
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
+      title: 'GIT - Training - Udemy/DoKodu [2019, 2022]',
+      period: '1600 – 1601',
+      skills: ['comedy', 'love'],
       contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
+Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
 `
-    }
+    },
   ],
   experience: [
     {
