e548af9585c8d0a3d3739650c5c498864491db9e
Monika Wiśniewska <wisniewska.monika00@gmail.com>
2023-10-29 20:58:29 +0100

    update additional info

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -29,62 +29,26 @@ module.exports = {
     ['English', 75],
     ['Spanish', 25],
   ],
-  positions: [
+  exprience: [
     {
-      title: 'Type Annotations in Python - Training - Udemy [2023]',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      header: 'Type Annotations in Python - Training - Udemy [2023]',
+      skills: ['python', 'typing'],
     },
     {
-      title: 'Summer of Code - Challenge (Python, HTML, CSS, WebScraping, API) - FlyNerd [2023]',
-      period: 'Likely early 17th century',
-      skills: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      header: 'Summer of Code - Challenge (Python, HTML, CSS, WebScraping, API) - FlyNerd [2023]',
+      skills: ['python', 'html', 'css'],
     },
     {
-      title: 'Elements of AI - Course - Univeristy of Helsinki/Jagiellonian University [2023]',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      header: 'Elements of AI - Course - Univeristy of Helsinki/Jagiellonian University [2023]',
+      skills: ['ai', 'introduction'],
     },
     {
-      title: 'New You in IT - Challenge - COI [2023]',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      header: 'New You in IT - Challenge (Testing, Agile, Scrum) - COI [2023]',
+      skills: ['agile', 'scrum', 'testing'],
     },
     {
-      title: 'GIT - Training - Udemy/DoKodu [2019, 2022]',
-      period: '1600 – 1601',
-      skills: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      header: 'GIT - Training - Udemy/DoKodu [2019, 2022]',
+      skills: ['git'],
     },
-  ],
-  experience: [
-    {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
-    },
-    {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. I dedicated them to Henry Wriothesley, Earl of Southampton.
-`
-    }
   ]
 };
