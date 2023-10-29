2ba0bd8c7d04cde1aabbe6d006aa7138c21539b0
Monika Wiśniewska <wisniewska.monika00@gmail.com>
2023-10-29 21:18:44 +0100

    update

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -29,6 +29,16 @@ module.exports = {
     ['English', 75],
     ['Spanish', 25],
   ],
+  positions: [
+    {
+      title: 'GIT - Training - Udemy/DoKodu [2019, 2022]',
+      period: '1600 – 1601',
+      skills: ['comedy', 'love'],
+      contents: `
+Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+`
+    },
+  ],
   experience: [
     {
       header: 'Type Annotations in Python - Training - Udemy [2023]',
